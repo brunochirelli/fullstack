7340ea0fdf35ec9d6e1401302302ea1e817ccb92
Bruno Chirelli <brunochirelli@gmail.com>
2022-01-16 17:52:09 -0300

    fix route to say hello

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello Cambada");
 });
 
 // Add route to say hello
-app.get("/hello:name", (req, res) => {
+app.get("/hello/:name", (req, res) => {
   res.send(`Hello ${req.params.name}`);
 });
 
