a9e5728ecc304f7fafb5fe4198310bfaf87d827b
Bruno Chirelli <brunochirelli@gmail.com>
2022-01-16 17:30:49 -0300

    add hello route

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-const express = require('express')
+const express = require("express");
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.listen(port, console.log(`Example app listening on port ${port}!`))
+// Add route to say hello
+app.get("/hello/:name", (req, res) => {
+  res.send(`Hello ${req.params.name}`);
+});
+
+app.listen(port, console.log(`Example app listening on port ${port}!`));
